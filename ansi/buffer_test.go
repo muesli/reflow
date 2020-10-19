d59e5ed49a097fda1ad3f664b5c4aadd9b27bad1
kiyon <kiyonlin@163.com>
2020-10-19 13:11:28 +0800

    Make all test cases parallel

--- ansi/buffer_test.go
+++ ansi/buffer_test.go
@@ -6,6 +6,8 @@ import (
 )
 
 func TestBuffer_PrintableRuneWidth(t *testing.T) {
+	t.Parallel()
+
 	var bb bytes.Buffer
 	bb.WriteString("\x1B[38;2;249;38;114mfoo")
 	b := Buffer{bb}
@@ -20,11 +22,11 @@ func Benchmark_PrintableRuneWidth(b *testing.B) {
 	s := "\x1B[38;2;249;38;114mfoo"
 	var n int
 
-	b.ReportAllocs()
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		n = PrintableRuneWidth(s)
-	}
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			n = PrintableRuneWidth(s)
+		}
+	})
 
 	if n != 3 {
 		b.Fatalf("width should be 3, got %d", n)
