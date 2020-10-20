16e9d43e9925b858272b56c330ff2a2e66e2c052
kiyon <kiyonlin@163.com>
2020-10-20 13:02:47 +0800

    Improve margin test cases

--- margin/margin_test.go
+++ margin/margin_test.go
@@ -1,7 +1,12 @@
 package margin
 
 import (
+	"errors"
 	"testing"
+
+	"github.com/muesli/reflow/indent"
+
+	"github.com/muesli/reflow/padding"
 )
 
 func TestMargin(t *testing.T) {
@@ -23,6 +28,13 @@ func TestMargin(t *testing.T) {
 			"foobar",
 			"  foobar  ",
 			10,
+			2,
+		},
+		// Asymmetric margin:
+		{
+			"foo",
+			"  foo ",
+			6,
 			2,
 		},
 		// Multi-line margin:
@@ -69,4 +81,45 @@ func TestMarginString(t *testing.T) {
 	if actual != expected {
 		t.Errorf("expected:\n\n`%s`\n\nActual Output:\n\n`%s`", expected, actual)
 	}
+}
+
+func BenchmarkMarginString(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		b.ReportAllocs()
+		b.ResetTimer()
+		for pb.Next() {
+			String("foobar", 10, 2)
+		}
+	})
+}
+
+func TestWriter_Error(t *testing.T) {
+	t.Parallel()
+
+	f := &Writer{
+		iw: indent.NewWriter(2, nil),
+		pw: padding.NewWriterPipe(fakeWriter{}, 10, nil),
+	}
+
+	if _, err := f.Write([]byte("foobar")); err != fakeErr {
+		t.Error(err)
+	}
+
+	f.iw = indent.NewWriterPipe(fakeWriter{}, 2, nil)
+
+	if _, err := f.Write([]byte("foobar")); err != fakeErr {
+		t.Error(err)
+	}
+
+	if err := f.Close(); err != fakeErr {
+		t.Error(err)
+	}
+}
+
+var fakeErr = errors.New("fake error")
+
+type fakeWriter struct{}
+
+func (fakeWriter) Write(_ []byte) (int, error) {
+	return 0, fakeErr
 }
