32a9121031a4a92012fb5fc2e10769cdb48440af
kiyon <kiyonlin@163.com>
2020-10-31 15:03:30 +0800

    Improve dedent performance (#16)

--- dedent/dedent_test.go
+++ dedent/dedent_test.go
@@ -26,6 +26,14 @@ func TestDedent(t *testing.T) {
 			Expected: "line 1\nline 2\nline 3\n\n",
 		},
 		{
+			Input:    " \tline 1\n\t\tline 2\n\t line 3\n\n",
+			Expected: "line 1\nline 2\nline 3\n\n",
+		},
+		{
+			Input:    "\t\tline 1\n\n\t\tline 2\n\tline 3",
+			Expected: "\tline 1\n\n\tline 2\nline 3",
+		},
+		{
 			Input:    "\n\n\n\n\n\n",
 			Expected: "\n\n\n\n\n\n",
 		},
@@ -41,4 +49,16 @@ func TestDedent(t *testing.T) {
 			t.Errorf("Test %d, expected:\n\n`%s`\n\nActual Output:\n\n`%s`", i, tc.Expected, s)
 		}
 	}
+}
+
+// go test -bench=BenchmarkDedent -benchmem -count=4
+func BenchmarkDedent(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		input := "  line 1\n\n  line 2\n line 3"
+		b.ReportAllocs()
+		b.ResetTimer()
+		for pb.Next() {
+			String(input)
+		}
+	})
 }
